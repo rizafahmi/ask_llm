5489d80f0378233064ad6866c2321da4579ba234
Riza Fahmi <rizafahmi@gmail.com>
2025-04-21 21:15:01 +0700

    Refactor testing: generating and displaying

--- lib/ask_llm.ex
+++ lib/ask_llm.ex
@@ -1,16 +1,19 @@
 defmodule AskLlm.Cli do
-  @help_message """
-    Usage: ask_llm [options] <question>
+  def generate_help_message() do
+    """
+      Usage: ask_llm [options] <question>
 
-    Options:
-      --help, -h       Show this help message
-      --model PROVIDER/MODEL    Specify the provider/model to use (default: anthropic/claude3.7-sonnet)
-  """
+      Options:
+        --help, -h       Show this help message
+        --model PROVIDER/MODEL    Specify the provider/model to use (default: anthropic/claude3.7-sonnet)
+    """
+  end
+
   def main([]) do
-    IO.puts(@help_message)
+    IO.puts(generate_help_message())
   end
 
   def main(["--help"]) do
-    IO.puts(@help_message)
+    IO.puts(generate_help_message())
   end
 end
--- test/ask_llm_test.exs
+++ test/ask_llm_test.exs
@@ -8,18 +8,22 @@ defmodule AskLlmTest do
 
     Options:
       --help, -h       Show this help message
-      --model PROVIDER/MODEL    Specify the provider/model to use (default: anthropic/claude3.7-sonnet)\n
+      --model PROVIDER/MODEL    Specify the provider/model to use (default: anthropic/claude3.7-sonnet)
   """
 
   test "generate help message if no args" do
-    assert capture_io(fn ->
-             AskLlm.Cli.main([])
-           end) == @help_message
+    assert AskLlm.Cli.generate_help_message() == @help_message
   end
 
-  test "generate help message with --help flag" do
+  test "display help message if no args" do
+    assert capture_io(fn ->
+             AskLlm.Cli.main([])
+           end) == @help_message <> "\n"
+  end
+
+  test "display help message with --help flag" do
     assert capture_io(fn ->
              AskLlm.Cli.main(["--help"])
-           end) == @help_message
+           end) == @help_message <> "\n"
   end
 end
